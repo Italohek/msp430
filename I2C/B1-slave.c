3bc8e7071723523aec0815d0845d5124a189b668
Italohek <132797328+Italohek@users.noreply.github.com>
2024-05-29 09:36:18 -0300

    Update B1-slave.c

--- I2C/B1-slave.c
+++ I2C/B1-slave.c
@@ -1,56 +1,64 @@
-#include <msp430.h> 
-#include "slave.h"
+// ER 9.3
+// Endereçar escravo e transmitir 0x55
+// Transmitir um único dado
+// Mestre -> USCI_B0: P3.0 = SDA e P3.1 = SCL
+// Escravo -> USCI_B1: P4.1 = SDA e P4.2 = SCL, endereço 42
+#include <msp430.h>
+#include "slave.h" //<--- ER 9.1
+#define TRUE 1
+#define FALSE 0
+# define BR10K 105 //com SMCLK
 
+// Funções usadas
 
-int main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
-	return 0;
+void teste_nack(void);
+void config_USCI_B0(void);
+void config_leds(void);
+
+int main(void) {
+    WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
+    config_USCI_B1(); //<--- ER 9.1, USCI_B1 como escrava
+    config_USCI_B0(); //USCI_B1 como mestre
+    config_leds(); //Configurar Leds
+    UCB0I2CSA = 42; //Endereço do escravo
+    UCB0CTL1 |= UCTR | UCTXSTT; //Gerar START
+    while ( (UCB0IFG & UCTXIFG) == 0); //Esperar TXIFG=1 (START)
+    UCB0TXBUF=0x55; //Primeiro número no TXBUF
+    while ( (UCB0CTL1 & UCTXSTT) == UCTXSTT); //Esperar TXSTT=0 (confirmação)
+    teste_nack();
+    UCB0CTL1 |= UCTXSTP; //Gerar STOP
+    while ( (UCB0CTL1 & UCTXSTP) == UCTXSTP); //Esperar STOP
+    P4OUT |= BIT7; //Verde indica sucesso
+    while(TRUE); //Prender execução
+    return 0;
 }
 
-void USCI_B1_config(void) {
-    UCB1CTL1 = UCSWRST;             // Resetar USCI_B1
-    UCB1CTL0 = UCMODE_3 | UCSYNC;   // Modo I2C síncrono
-    UCB1I2CA = 52;                  // Endereço do escravo que em hexa é 0x34
-    UCB1CTL1 = 0;                   // UCSWRST = 0
-    UCB1IE = UCTXIE | UCRXIE;       // Hablita interrupção TX e RX
-    P4SEL |= BIT2 | BIT1;           // Função alternativa
-    P4REN |= BIT2 | BIT1;           // Habilita o resistor
-    P4OUT |= BIT2 | BIT1;           // Habilita o pullup
-    for (b1 = 0; b1<MAX_USCI_B1; b1++){ // Zera o vetor de recepção
-        b1_vet[b1] = 0;
-    }
-    b1 = 0;                         // Zera o indexador
-    b1_cont = 0;                    // Zera o contador
-    __enable_interrupt();           // Habilita interrupções
-}
-
-// ISR da USCI_B1
-#pragma vector = USCI_B1_VECTOR     // Vetor 45
-__interrupt void ist_USCI_B1(void) {
-    int n;
-    n = __even_in_range(UCB1IV, 0xC);
-    switch(n) {
-    case 0x0: break;
-    case 0x2: break;
-    case 0x4: break;
-    case 0x6: break;
-    case 0x8: break;
-    case 0xA: USCI_B1_rx(); break;  // Rotina RX
-    case 0xC: USCI_B1_tx(); break;  // Rotina TX
-    }
-}
-
-// Rotina para receber um byte e guardar no vetor
-void USCI_B1_rx(void) {
-    b1_vet[b1++] = UCB1RXBUF;       // Guarda o dado recebido
-    if (b1 > MAX_USCI_B1) {
-        b1 = 0;
+//Verificar se aconteceu NACK
+void teste_nack(void){
+    if ((UCB0IFG & UCNACKIFG) == UCNACKIFG){
+        P1OUT |= BIT0; //Vermelho=falha
+        UCB0CTL1 |= UCTXSTP; //Gerar STOP
+        while ( (UCB0CTL1 & UCTXSTP) == UCTXSTP); //Esperar STOP
+        while(1); //Travar
     }
 }
 
-// Rotina para transmitir o contador e incrementá-lo
-void USCI_B1_tx(void) {
-    UCB1TXBUF = b1_cont++;
+// Configurar USCI_B0 como mestre
+// P3.0 = SDA e P3.1 = SCL
+void config_USCI_B0(void){
+    UCB0CTL1 = UCSWRST; //Ressetar USCI_B1
+    UCB0CTL0 = UCMST | //Modo Mestre
+    UCMODE_3 | //I2C
+    UCSYNC; //Síncrono
+    UCB0BRW = BR10K; //10 kbps
+    UCB0CTL1 = UCSSEL_3; //SMCLK e UCSWRST=0
+    P3SEL |= BIT1 | BIT0; //Funções alternativas
+    P3REN |= BIT1 | BIT0;
+    P3OUT |= BIT1 | BIT0;
+}
+
+// Configurar Leds
+void config_leds(void){
+    P1DIR |= BIT0; P1OUT &= ~BIT0;
+    P4DIR |= BIT7; P4OUT &= ~BIT7;
 }
