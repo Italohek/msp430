1b2be2225d3b47541aed1e7e2c8ebc32e4946893
Italohek <132797328+Italohek@users.noreply.github.com>
2024-05-29 09:35:59 -0300

    Update slave.h

--- I2C/slave.h
+++ I2C/slave.h
@@ -2,7 +2,7 @@
  * slave.h
  *
  *  Created on: 28 de mai de 2024
- *      Author: Hekazinho
+ *      Author: UNB
  */
 
 #ifndef SLAVE_H_
@@ -15,7 +15,7 @@ void USCI_B1_rx(void);
 void USCI_B1_tx(void);
 
 // Variáveis globais
-volatile char b1_vet(MAX_B1); // Vetor para receber os dados
+volatile char b1_vet[MAX_B1]; // Vetor para receber os dados
 volatile char b1; // Indexador para o vetor
 volatile char b1_cont; // Contador para quando for transmitir dados
 
