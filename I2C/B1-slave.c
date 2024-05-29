a1039cce87be5248c217722ba2a034df904b5a6e
Italohek <132797328+Italohek@users.noreply.github.com>
2024-05-29 10:10:23 -0300

    Update B1-slave.c

--- I2C/B1-slave.c
+++ I2C/B1-slave.c
@@ -1,52 +1,66 @@
-// ER 9.3
 // Endereçar escravo e transmitir 0x55
 // Transmitir um único dado
 // Mestre -> USCI_B0: P3.0 = SDA e P3.1 = SCL
 // Escravo -> USCI_B1: P4.1 = SDA e P4.2 = SCL, endereço 42
 #include <msp430.h>
-#include "slave.h" //<--- ER 9.1
+#include "slave.h"
 #define TRUE 1
 #define FALSE 0
 # define BR10K 105 //com SMCLK
 
 // Funções usadas
 
-void teste_nack(void);
+int teste_nack(void);
 void config_USCI_B0(void);
-void config_leds(void);
 
 int main(void) {
     WDTCTL = WDTPW | WDTHOLD; // stop watchdog timer
-    config_USCI_B1(); //<--- ER 9.1, USCI_B1 como escrava
+    USCI_B1_config(); //<--- USCI_B1 como escrava
     config_USCI_B0(); //USCI_B1 como mestre
-    config_leds(); //Configurar Leds
-    UCB0I2CSA = 42; //Endereço do escravo
-    UCB0CTL1 |= UCTR | UCTXSTT; //Gerar START
-    while ( (UCB0IFG & UCTXIFG) == 0); //Esperar TXIFG=1 (START)
-    UCB0TXBUF=0x55; //Primeiro número no TXBUF
-    while ( (UCB0CTL1 & UCTXSTT) == UCTXSTT); //Esperar TXSTT=0 (confirmação)
-    teste_nack();
-    UCB0CTL1 |= UCTXSTP; //Gerar STOP
-    while ( (UCB0CTL1 & UCTXSTP) == UCTXSTP); //Esperar STOP
-    P4OUT |= BIT7; //Verde indica sucesso
-    while(TRUE); //Prender execução
-    return 0;
+    UCB0I2CSA = 0x37; //Endereço do escravo
+    volatile int i = 0;
+    volatile char a;
+    for(i = 0; i < 128; i++) {
+        UCB0CTL1 |= UCTR | UCTXSTT; //Gerar START
+        while ( (UCB0IFG & UCTXIFG) == 0); //Esperar TXIFG=1 (START)
+        UCB0TXBUF=0x55; //Primeiro número no TXBUF
+        while ( (UCB0CTL1 & UCTXSTT) == UCTXSTT); //Esperar TXSTT=0 (confirmação)
+        //teste_nack();
+        UCB0CTL1 |= UCTXSTP; //Gerar STOP
+        while ( (UCB0CTL1 & UCTXSTP) == UCTXSTP); //Esperar STOP
+        if (!teste_nack()) {
+            a = i;
+            break;
+        }
+    }
+    while(1);
 }
 
 //Verificar se aconteceu NACK
-void teste_nack(void){
-    if ((UCB0IFG & UCNACKIFG) == UCNACKIFG){
-        P1OUT |= BIT0; //Vermelho=falha
-        UCB0CTL1 |= UCTXSTP; //Gerar STOP
-        while ( (UCB0CTL1 & UCTXSTP) == UCTXSTP); //Esperar STOP
-        while(1); //Travar
+int teste_nack(void){
+    if ((UCB0IFG & UCNACKIFG) == 0){
+        return 0;
     }
+    return 1;
 }
 
+void USCI_B1_config(void){
+    UCB1CTL1 = UCSWRST; //Resetar USCI_B1
+    UCB1CTL0 = UCMODE_3 | //Modo I2C
+    UCSYNC; //Síncrono
+    UCB1I2COA = 0x37; //Endereço do Escravo
+    UCB1CTL1 = 0; //UCSWRST=0
+    UCB1IE = UCTXIE | UCRXIE; //Hab interrp TX e RX
+    P4SEL |= BIT2 | BIT1; //Função alternativa
+    P4REN |= BIT2 | BIT1; //Hab resistor
+    P4OUT |= BIT2 | BIT1; //Pullup
+    }
+
 // Configurar USCI_B0 como mestre
+
 // P3.0 = SDA e P3.1 = SCL
 void config_USCI_B0(void){
-    UCB0CTL1 = UCSWRST; //Ressetar USCI_B1
+    UCB0CTL1 = UCSWRST; //Resetar USCI_B1
     UCB0CTL0 = UCMST | //Modo Mestre
     UCMODE_3 | //I2C
     UCSYNC; //Síncrono
@@ -57,8 +71,4 @@ void config_USCI_B0(void){
     P3OUT |= BIT1 | BIT0;
 }
 
-// Configurar Leds
-void config_leds(void){
-    P1DIR |= BIT0; P1OUT &= ~BIT0;
-    P4DIR |= BIT7; P4OUT &= ~BIT7;
-}
+
