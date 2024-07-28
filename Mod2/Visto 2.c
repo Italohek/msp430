5aa8d53ab3a816bfb5926f9b0e05464391a65623
Italohek <132797328+Italohek@users.noreply.github.com>
2024-07-27 21:58:12 -0300

    Update Visto 2.c

--- Mod2/Visto 2.c
+++ Mod2/Visto 2.c
@@ -1,269 +1,259 @@
 #include <msp430.h>
 
-void config_ta1_1(void);
-void instructions(int tecla);
-void config_leds(void);
-void config_tb0_1(void);
-void config_servo(void);
-void config_rising_ta1_1(void);
-void config_falling_ta1_1(void);
-void wait_first_falling_edge(void);
-void wait_second_falling_edge(void);
-void wait_rising_edge(void);
-void write_vector(void);
-void vector_into_words(void);
-void debounce(int valor);
+void configure_ta1_capture(void);
+void execute_instructions(int button);
+void configure_leds(void);
+void configure_tb0_pwm(void);
+void configure_servo(void);
+void set_ta1_rising_edge_capture(void);
+void set_ta1_falling_edge_capture(void);
+void await_first_falling_edge(void);
+void await_second_falling_edge(void);
+void await_rising_edge(void);
+void record_capture_values(void);
+void decode_vector_to_word(void);
+void debounce_delay(int value);
 
-void up_pos(void);
-void down_pos(void);
+void increase_servo_position(void);
+void decrease_servo_position(void);
 
-#define _1_x_1 0XBA45
-#define _1_x_2 0XB946
-#define _1_x_3 0XB847
+#define BUTTON_1_1 0XBA45
+#define BUTTON_1_2 0XB946
+#define BUTTON_1_3 0XB847
 
-#define _2_x_1 0XBB44
-#define _2_x_2 0XBF40
-#define _2_x_3 0XBC43
+#define BUTTON_2_1 0XBB44
+#define BUTTON_2_2 0XBF40
+#define BUTTON_2_3 0XBC43
 
-#define _3_x_1 0XF807
-#define _3_x_2 0XEA15
-#define _3_x_3 0XF609
+#define BUTTON_3_1 0XF807
+#define BUTTON_3_2 0XEA15
+#define BUTTON_3_3 0XF609
 
-#define _4_x_1 0XE916
-#define _4_x_2 0XE619
-#define _4_x_3 0XF20D
+#define BUTTON_4_1 0XE916
+#define BUTTON_4_2 0XE619
+#define BUTTON_4_3 0XF20D
 
-#define _5_x_2 0XE718
+#define BUTTON_5_2 0XE718
 
-#define _6_x_1 0XF708
-#define _6_x_2 0XE31C
-#define _6_x_3 0XA55A
+#define BUTTON_6_1 0XF708
+#define BUTTON_6_2 0XE31C
+#define BUTTON_6_3 0XA55A
 
-#define _7_x_2 0XAD52
+#define BUTTON_7_2 0XAD52
 
-#define POT100 10000 //0,02 * 1.048.576 --> 10 ms
+#define PERIOD_10MS 10000
 
-#define partida_MAX 14500
-#define partida_MIN 13500
-#define bit1_MAX 2450
-#define bit1_MIN 2250
-#define bit0_MAX 1120
-#define bit0_MIN 1340
+#define START_MAX 14500
+#define START_MIN 13500
+#define BIT_1_MAX 2450
+#define BIT_1_MIN 2250
+#define BIT_0_MAX 1120
+#define BIT_0_MIN 1340
 
-#define T20ms 20971 //Período 20 ms (1.048.576 x 0,02)
-#define T500us 524 //Período 0,5 ms (1.048.576 x 0,0005)
-#define P10GRAUS 117 //Passo de 10 graus (2.097 / 18) 116.5 =~ 117
+#define PERIOD_20MS 20971
+#define PERIOD_500US 524
+#define STEP_10_DEGREES 117
 
-volatile int i = 0;
-volatile int period=0;
-volatile int vector[32];
-volatile unsigned long code; //variavel
-volatile unsigned long tecla;
-volatile int pos=0;
+volatile int index = 0;
+volatile int pulse_period = 0;
+volatile int capture_vector[32];
+volatile unsigned long decoded_code;
+volatile unsigned long button_code;
+volatile int servo_position = 0;
 
 int main(void)
 {
-    WDTCTL = WDTPW | WDTHOLD;   // Desabilita o watchdog timer
-    config_leds(); // Configuração dos LEDs
-    config_tb0_1(); // Configuração do Timer_B0 para PWM
-    config_ta1_1(); // Configuração do Timer_A1 para captura de flancos
-    config_servo(); // Configuração do Timer_A2 para controle de servo
-    while(1){
-        config_falling_ta1_1(); // Configuração de captura de flanco de descida
-        wait_first_falling_edge(); // Espera pelo primeiro flanco de descida
-        wait_second_falling_edge(); // Espera pelo segundo flanco de descida
-        if((period>partida_MIN) && (period<partida_MAX)) { // Verifica período
-            config_rising_ta1_1(); // Configuração de captura de flanco de subida
-            wait_rising_edge(); // Espera pelo flanco de subida
-            write_vector(); // Escreve valores de captura no vetor
+    WDTCTL = WDTPW | WDTHOLD;   // Disable watchdog timer
+    configure_leds();
+    configure_tb0_pwm();
+    configure_ta1_capture();
+    configure_servo();
+    
+    while (1) {
+        set_ta1_falling_edge_capture();
+        await_first_falling_edge();
+        await_second_falling_edge();
+        
+        if ((pulse_period > START_MIN) && (pulse_period < START_MAX)) {
+            set_ta1_rising_edge_capture();
+            await_rising_edge();
+            record_capture_values();
         }
-        vector_into_words(); // Converte vetor em palavras
-        instructions(tecla); // Executa instruções com base na tecla pressionada
+        
+        decode_vector_to_word();
+        execute_instructions(button_code);
     }
 }
 
-// Função para escrever valores de captura no vetor
-void write_vector(void){
-    for(i=0;i<32;i++){
-        while((TA1CCTL1 & CCIFG)==0); // Aguarda flag de captura
-        TA1CTL |= TACLR; // Limpa o timer
-        TA1CCTL1 &=~CCIFG; // Limpa flag de captura
-        vector[i]=TA1CCR1; // Armazena valor de captura no vetor
+void record_capture_values(void) {
+    for (index = 0; index < 32; index++) {
+        while (!(TA1CCTL1 & CCIFG));
+        TA1CTL |= TACLR;
+        TA1CCTL1 &= ~CCIFG;
+        capture_vector[index] = TA1CCR1;
     }
 }
 
-// Função para converter vetor em palavras
-void vector_into_words(void){
-    for (i=0;i<32;i++){
-        code=code>>1; // Desloca código para a direita
-        if((vector[i]>bit1_MIN) && (vector[i]<bit1_MAX)){
-            code |= 0x80000000L; // Atualiza bit mais significativo do código
+void decode_vector_to_word(void) {
+    for (index = 0; index < 32; index++) {
+        decoded_code >>= 1;
+        if ((capture_vector[index] > BIT_1_MIN) && (capture_vector[index] < BIT_1_MAX)) {
+            decoded_code |= 0x80000000L;
         }
     }
-    tecla=code>>16; // Atualiza código da tecla
+    button_code = decoded_code >> 16;
 }
 
-void config_ta1_1(void){
-    //Configuracao de TA1.1 e P2.0
-    TA1CTL = TASSEL_2 | MC_2; //SMCLK, Modo 2
-    P2DIR &= ~BIT0; //P2.0 como entrada
-    P2REN |= BIT0; //Habilitar resistor
-    P2OUT |= BIT0; //Para pullup
-    P2SEL |= BIT0; //Dedicado para captura (TA1.1)
-    TA1CCTL1 &= ~CCIFG; //Apagar CCIFG
+void configure_ta1_capture(void) {
+    TA1CTL = TASSEL_2 | MC_2; // SMCLK, Continuous mode
+    P2DIR &= ~BIT0; // P2.0 as input
+    P2REN |= BIT0; // Enable pull-up resistor
+    P2OUT |= BIT0;
+    P2SEL |= BIT0; // P2.0 as capture input for TA1.1
+    TA1CCTL1 &= ~CCIFG;
 }
 
-// Configuração do Timer_A1 para captura de flanco de subida
-void config_rising_ta1_1(void){
-    TA1CCTL1 = CM_1 | CCIS_0 | SCS | CAP; // Captura flanco de subida
-    TA1CCTL1 &= ~CCIFG; // Limpa flag de captura
+void set_ta1_rising_edge_capture(void) {
+    TA1CCTL1 = CM_1 | CCIS_0 | SCS | CAP;
+    TA1CCTL1 &= ~CCIFG;
 }
 
-// Configuração do Timer_A1 para captura de flanco de descida
-void config_falling_ta1_1(void){
-    TA1CCTL1 = CM_2 | CCIS_0 | SCS | CAP; // Captura flanco de descida
-    TA1CCTL1 &= ~CCIFG; // Limpa flag de captura
+void set_ta1_falling_edge_capture(void) {
+    TA1CCTL1 = CM_2 | CCIS_0 | SCS | CAP;
+    TA1CCTL1 &= ~CCIFG;
 }
 
-// Espera pelo primeiro flanco de descida
-void wait_first_falling_edge(void){
-    while((TA1CCTL1&CCIFG)==0); // Aguarda primeiro flanco de descida
-    TA1CTL|=TACLR; // Limpa o timer
-    TA1CCTL1&=~CCIFG; // Limpa flag de captura
+void await_first_falling_edge(void) {
+    while (!(TA1CCTL1 & CCIFG));
+    TA1CTL |= TACLR;
+    TA1CCTL1 &= ~CCIFG;
 }
 
-// Espera pelo segundo flanco de descida
-void wait_second_falling_edge(void){
-    while((TA1CCTL1&CCIFG)==0); // Aguarda segundo flanco de descida
-    TA1CTL|=TACLR; // Limpa o timer
-    TA1CCTL1&=~CCIFG; // Limpa flag de captura
-    period = TA1CCR1; // Atualiza período
+void await_second_falling_edge(void) {
+    while (!(TA1CCTL1 & CCIFG));
+    TA1CTL |= TACLR;
+    TA1CCTL1 &= ~CCIFG;
+    pulse_period = TA1CCR1;
 }
 
-// Espera pelo flanco de subida
-void wait_rising_edge(void){
-    while((TA1CCTL1&CCIFG)==0); // Aguarda flanco de subida
-    TA1CTL|=TACLR; // Limpa o timer
-    TA1CCTL1&=~CCIFG; // Limpa flag de captura
+void await_rising_edge(void) {
+    while (!(TA1CCTL1 & CCIFG));
+    TA1CTL |= TACLR;
+    TA1CCTL1 &= ~CCIFG;
 }
 
-// Configuração dos LEDs
-void config_leds(void){
-    P1OUT &= ~BIT0; // LED vermelho desligado
-    P1DIR |= BIT0; // LED vermelho como saída
-    P4OUT &= ~BIT7; // LED verde desligado
-    P4DIR |= BIT7; // LED verde como saída
-    P4SEL |= BIT7; // P4.7 mapeado para saída de Timer_B0
-    PMAPKEYID = 0X02D52; //Escrever chave
-    P4MAP7 = PM_TB0CCR1A; //TB0.1 mapeado para P4.7
+void configure_leds(void) {
+    P1OUT &= ~BIT0;
+    P1DIR |= BIT0;
+    P4OUT &= ~BIT7;
+    P4DIR |= BIT7;
+    P4SEL |= BIT7;
+    PMAPKEYID = 0X02D52;
+    P4MAP7 = PM_TB0CCR1A;
 }
 
-
-// Incrementa posição do servo
-void up_pos(void) {
-    pos++;
-    if (pos > 18) pos=18;
-    TA2CCR2=T500us+pos*P10GRAUS; // Atualiza posição do servo
+void increase_servo_position(void) {
+    servo_position++;
+    if (servo_position > 18) servo_position = 18;
+    TA2CCR2 = PERIOD_500US + servo_position * STEP_10_DEGREES;
 }
 
-// Decrementa posição do servo
-void down_pos(void) {
-    pos--;
-    if (pos < 0) pos=0;
-    TA2CCR2=T500us+pos*P10GRAUS; // Atualiza posição do servo
+void decrease_servo_position(void) {
+    servo_position--;
+    if (servo_position < 0) servo_position = 0;
+    TA2CCR2 = PERIOD_500US + servo_position * STEP_10_DEGREES;
 }
 
-void instructions(int tecla){
-    switch(tecla){
-        case _1_x_1:
+void execute_instructions(int button) {
+    switch(button) {
+        case BUTTON_1_1:
             P1OUT |= BIT0;
             break;
 
-        case _1_x_2:
+        case BUTTON_1_2:
             P1OUT &= ~BIT0;
             break;
 
-        case _1_x_3:
+        case BUTTON_1_3:
             P1OUT ^= BIT0;
             break;
 
-        case _2_x_1:
-            TB0CCR1 = POT100;
+        case BUTTON_2_1:
+            TB0CCR1 = PERIOD_10MS;
             break;
 
-        case _2_x_2:
+        case BUTTON_2_2:
             TB0CCR1 = 0;
             break;
 
-        case _2_x_3:
-            TB0CCR1 = POT100 - TB0CCR1;
-            debounce(10000);
+        case BUTTON_2_3:
+            TB0CCR1 = PERIOD_10MS - TB0CCR1;
+            debounce_delay(10000);
             break;
 
-        case _3_x_1:
-            TB0CCR1 = POT100 * 0.1;
+        case BUTTON_3_1:
+            TB0CCR1 = PERIOD_10MS * 0.1;
             break;
 
-        case _3_x_2:
-            TB0CCR1 = POT100 * 0.2;
+        case BUTTON_3_2:
+            TB0CCR1 = PERIOD_10MS * 0.2;
             break;
 
-         case _3_x_3:
-            TB0CCR1 = POT100 * 0.3;
+        case BUTTON_3_3:
+            TB0CCR1 = PERIOD_10MS * 0.3;
             break;
 
-         case _4_x_1:
-             TB0CCR1 = POT100 * 0.4;
-             break;
+        case BUTTON_4_1:
+            TB0CCR1 = PERIOD_10MS * 0.4;
+            break;
 
-         case _4_x_2:
-             TB0CCR1 = POT100 * 0.5;
-             break;
+        case BUTTON_4_2:
+            TB0CCR1 = PERIOD_10MS * 0.5;
+            break;
 
-         case _4_x_3:
-             TB0CCR1 = POT100 * 0.6;
-             break;
+        case BUTTON_4_3:
+            TB0CCR1 = PERIOD_10MS * 0.6;
+            break;
 
-         case _5_x_2:
-             TB0CCR1 = POT100 * 0.7;
-             break;
+        case BUTTON_5_2:
+            TB0CCR1 = PERIOD_10MS * 0.7;
+            break;
 
-         case _6_x_1:
-             down_pos();
-             break;
+        case BUTTON_6_1:
+            decrease_servo_position();
+            break;
 
-         case _6_x_2:
-             TB0CCR1 = POT100 * 0.8;
-             break;
+        case BUTTON_6_2:
+            TB0CCR1 = PERIOD_10MS * 0.8;
+            break;
 
-         case _6_x_3:
-             up_pos();
-             break;
+        case BUTTON_6_3:
+            increase_servo_position();
+            break;
 
-         case _7_x_2:
-             TB0CCR1 = POT100 * 0.9;
-             break;
+        case BUTTON_7_2:
+            TB0CCR1 = PERIOD_10MS * 0.9;
+            break;
     }
 }
 
-void config_tb0_1(void){
-    TB0CTL = TBSSEL_2 | MC_1; //TB0 com SMCLK e Modo Up
-    TB0CCR0 = POT100; //Período PWM
-    TB0CCTL1 = OUTMOD_6; //Saída Modo 6
-    TB0CCR1 = POT100; //Iniciar com 100%
+void configure_tb0_pwm(void) {
+    TB0CTL = TBSSEL_2 | MC_1; // TB0 with SMCLK and Up mode
+    TB0CCR0 = PERIOD_10MS;
+    TB0CCTL1 = OUTMOD_6;
+    TB0CCR1 = PERIOD_10MS;
 }
 
-void config_servo(void){
-    TA2CTL = TASSEL_2|ID_0|MC_1|TACLR;
+void configure_servo(void) {
+    TA2CTL = TASSEL_2 | ID_0 | MC_1 | TACLR;
     TA2EX0 = TAIDEX_0;
-    TA2CCR0 = T20ms; //Período de 20 ms
+    TA2CCR0 = PERIOD_20MS;
     TA2CCTL2 = OUTMOD_6;
-    TA2CCR2 = T500us; //Zero graus
-    //Configurar P2.5 para PWM
+    TA2CCR2 = PERIOD_500US;
     P2DIR |= BIT5;
-    P2SEL |= BIT5; //P2.5 = PWM
+    P2SEL |= BIT5;
 }
 
-void debounce(int valor) {
-    for(i = 0; i < valor; i++);
+void debounce_delay(int value) {
+    for (index = 0; index < value; index++);
 }
